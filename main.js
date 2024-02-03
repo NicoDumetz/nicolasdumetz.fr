e84048f99e11386192042f21694d3d9c8371c7e8
NicoDumetz <nicolas.dumetz@epitech.eu>
2024-02-03 16:56:43 +0100

    [~] add three js and second preload of site

---
+++ main.js
@@ -0,0 +1,81 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x000000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+camera.position.set(0,0,300);
+const loader = new GLTFLoader();
+
+loader.load( 'space/scene.gltf', (gltf) => {
+        const model = gltf.scene;
+        model.position.set(-100, -100, 100);
+        scene.add(model);
+        let caca = 0;
+        const animate = function () {
+            requestAnimationFrame(animate);
+            if (caca == 0) {
+                camera.position.z -= 0.5;
+                camera.rotation.x -= 0.01;
+            }
+            if (caca == 1) {
+                camera.position.z += 0.5;
+                camera.rotation.y += 0.01;
+                camera.rotation.x -= 0.01;
+            }
+            if (camera.position.z <= -190)
+                caca = 1;
+            if (caca ==  1 && camera.position.z >= 200)
+                caca = 0;
+            renderer.render(scene, camera);
+        };
+        animate();
+    },
+);
+
+
+// const scene = new THREE.Scene();
+// const camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 3000);
+
+// camera.position.z = 100;
+
+// let render = new THREE.WebGLRenderer()
+
+// render.setClearColor(0x000000)
+// render.setSize(window.innerWidth, window.innerHeight)
+
+// document.body.appendChild(render.domElement)
+
+
+// let forme = new THREE.Group()
+// let geometri = new THREE.TorusKnotGeometry(10, 3, 100, 16)
+
+// let materiel = new THREE.MeshNormalMaterial({
+//     color: 0xff0000,
+//     opacity: 1,
+//     wireframe: true,
+//     wireframeLinewidth: 5,
+//     wireframeLinejoin: 'round',
+//     wireframeLinecap: 'round',
+// });
+
+// forme.add(new THREE.Mesh(geometri, materiel))
+
+// scene.add(forme)
+
+
+
+// let animer = function() {
+//     requestAnimationFrame(animer)
+
+//     forme.rotation.x += 0.005
+//     forme.rotation.y += 0.005
+//     render.render(scene, camera)
+// }
+// animer()
+
+// render.render(scene, camera)
---
+++ preload/preload.js
@@ -0,0 +1,75 @@
+gsap.from(".loader-1", {
+    width: 0,
+    duration: 2,
+    ease: "power2.inOut",
+});
+
+gsap.from(".loader-2", {
+    width: 0,
+    duration: 2,
+    ease: "power2.inOut",
+});
+
+gsap.from(".loader-3", {
+    width: 0,
+    duration: 2,
+    ease: "power2.inOut",
+});
+
+gsap.to(".loader", {
+    background: "none",
+    delay: 2.5,
+    duration: 0.1,
+});
+
+gsap.to(".loader-1", {
+    rotate: 90,
+    y: 0,
+    x: "65%",
+    duration: 0.5,
+    delay: 2.5,
+});
+
+gsap.to(".loader-2", {
+    rotate: 46.75,
+    x: 0,
+    y: 0,
+    duration: 0.5,
+}, "<");
+
+gsap.to(".loader-3", {
+    rotate: 90,
+    x: "-65%",
+    y: 0,
+    duration: 0.5,
+}, "<");
+
+gsap.to(".loader", {
+    delay: 4,
+    scale: 50,
+    duration: 1.5,
+    ease: "power2.inOut",
+});
+
+gsap.to(".loader", {
+    rotate: 65,
+    duration: 1,
+    delay: 4,
+    ease: "power2.inOut",
+});
+
+gsap.to(".loading-screen", {
+    opacity: 0,
+    duration: 0.5,
+    delay: 4.5,
+    ease: "power1.inOut",
+});
+
+// gsap.to("canvas", 1.5, {
+//     delay: 4,
+//     y: -90,
+//     ease: "power4.inOut",
+//     stagger: {
+//         amount: 0.1,
+//     },
+// })
